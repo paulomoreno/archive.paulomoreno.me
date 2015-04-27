6ecac514612ad481efb520631a6f38e999695d4b
Paulo Moreno <paulomorenoo@gmail.com>
2015-04-27 12:23:34 -0300

    Organiza repo pt2

--- django/env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/paulomoreno/Documents/pessoal/site/paulomoreno.github.io/django/env/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
