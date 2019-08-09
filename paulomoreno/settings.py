d6763caf1226a02f39486e97a886ef71a07c8f96
paulomoreno <paulomorenoo@gmail.com>
2019-08-08 22:35:30 -0300

    Update settings.py

--- paulomoreno/settings.py
+++ paulomoreno/settings.py
@@ -8,6 +8,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
+from pathlib import Path
+
+PROJECT_DIR = Path(__file__).resolve().parents[2]
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
